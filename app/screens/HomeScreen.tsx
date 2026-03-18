d3b16b574ad8876b0e640b12615e4ba7f1c595e1
kenzkenzkenz <contact.kenzie@proton.me>
2026-03-18 15:53:02 -0500

    Handle submit click and call /match endpoint

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,15 +1,76 @@
 import { View, Text } from 'react-native';
+import React, { useState } from 'react';
 import Title from '@/components/Title';
 import PreferenceForm from '@/components/PreferenceForm';
+import ResultScreen from './ResultScreen';
+import { MatchResponse, Country } from '@/types';
 
 export default function HomeScreen() {
+    const [status, setStatus] = useState<'idle' | 'loading' | 'success' | 'error' | 'no-data' | 'rate-limit' | 'forbidden'>('idle');
+    const [match, setMatch] = useState<MatchResponse | null>(null);
+
+    const backendUrl = 'https://dream-weather-backend.onrender.com';
+
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    const handleSubmit = async (values: {
+        precipitation: string;
+        temperature: string;
+        country: Country
+    }) => {
+        setStatus('loading');
+
+        const reqBody = {
+            country: values.country,
+            precipitation: values.precipitation,
+            temperature: values.temperature
+        };
+
+        try {
+            const response = await fetch(`${backendUrl}/api/v1/livestream/match`, {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(values)
+            });
+
+            if (!response.ok) {
+                if (response.status === 404) {
+                    setStatus('no-data');
+                }
+                else if (response.status === 403) {
+                    setStatus('forbidden');
+                }
+                else if (response.status === 429) {
+                    setStatus('rate-limit');
+                }
+                else {
+                    setStatus('error');
+                }
+                return;
+            }
+
+            const data = await response.json();
+            console.log("response body: ", data);
+            if (!data) {
+                setStatus('no-data');
+                return;
+            }
+
+            setMatch(data);
+            setStatus('success');
+        } catch (error) {
+            setStatus('error');
+        }
+    };
 
     return (
         <View style={{ alignItems: 'center', justifyContent: 'flex-start', paddingBottom: 20 }}>
             <Title />
             <View style={{ margin: 20 }}>
-                <Text>Tell us what vibe you want, and we'll send you to the perfect U.S. location.</Text>
-                <PreferenceForm />
+                {status === 'idle' && <PreferenceForm onSubmit={handleSubmit} />}
+                {status === 'success' && <ResultScreen match={match} />}
             </View>
         </View>
     )
