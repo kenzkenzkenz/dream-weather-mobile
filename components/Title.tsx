91b12b34edbf08bf306aba542fe5b6e12cf951a4
kenzkenzkenz <contact.kenzie@proton.me>
2026-03-18 15:51:57 -0500

    Format Title

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,16 +2,16 @@ import { View, Text, Image } from 'react-native';
 
 export default function Title() {
     return (
-        <View style={{ alignItems: 'center' }}>
-            <Image
-                source={require('../assets/images/logo.png')}
-                style={{ width: 100, height: 100, marginBottom: 20 }}
-            />
-            <View>
-                <Text
-                    style={{ fontSize: 24, fontWeight: 'bold' }}
-                >Dream Weather</Text>
-                <Text style={{ fontStyle: 'italic', fontSize: 16 }}>Mobile</Text>
+        <View>
+            <View style={{ alignItems: 'center', flexDirection: 'row' }}>
+                <Image
+                    source={require('../assets/images/logo.png')}
+                    style={{ width: 80, height: 80, marginRight: 10 }}
+                />
+                <View>
+                    <Text style={{ fontSize: 32, fontWeight: 'bold' }}>Dream Weather</Text>
+                    <Text style={{ fontStyle: 'italic', fontSize: 16 }}> Mobile</Text>
+                </View>
             </View>
         </View>
     )
