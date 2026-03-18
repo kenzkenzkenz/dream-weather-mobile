e4e4d2cca687a592ebf11d2b94438fe1cdb16837
kenzkenzkenz <contact.kenzie@proton.me>
2026-03-17 22:21:05 -0500

    Create CustomButton component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+type Country = {
+    name: string;
+    iso_code: string;
+}
+
+type Props = {
+    title: string;
+    onPress: (values: { country: Country; precip: string; temp: string }) => void;
+    values: { country: Country; precip: string; temp: string };
+};
+
+export default function CustomButton({ title, onPress, values }: Props) {
+    return (
+        <TouchableOpacity style={styles.button} onPress={() => onPress(values)}>
+            <Text style={styles.text}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#b6b6b6',
+        borderRadius: 8,
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        borderWidth: 0,
+        borderColor: 'black',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    text: {
+        color: 'black',
+        fontSize: 16,
+        fontWeight: '500'
+    },
+});
