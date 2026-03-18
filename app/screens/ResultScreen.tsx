df8ef265bf6d93c195c1464daf7f1f33b607fd0f
kenzkenzkenz <contact.kenzie@proton.me>
2026-03-18 15:53:27 -0500

    Set up results screen

--- app/screens/ResultScreen.tsx
+++ app/screens/ResultScreen.tsx
@@ -1,8 +1,17 @@
 import { View, Text, Button, Image } from 'react-native';
+import { MatchResponse } from '@/types';
 
+type Props = {
+    match: MatchResponse | null;
+}
 
-export default function ResultScreen() {
-    return(
-        <Text>Results here</Text>
+export default function ResultScreen({ match }: Props) {
+    return (
+        <View style={{ alignItems: 'center' }}>
+            <Text style={{ fontSize: 20, fontWeight: '500', margin: 10 }}>{match?.data?.title}</Text>
+            <Text style={{ margin: 10 }}>Current Conditions: {match?.data?.forecast.shortForecast}, {match?.data?.forecast.temperature}° F</Text>
+            <Text style={{ margin: 10 }}>{match?.data?.description}</Text>
+            
+        </View>
     )
 }
