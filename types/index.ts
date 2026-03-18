57ef3b6ee08eb37cca96b8a776552dbdfa9a5083
kenzkenzkenz <contact.kenzie@proton.me>
2026-03-18 15:49:30 -0500

    Define types in own file

---
+++ types/index.ts
@@ -0,0 +1,34 @@
+export type Country = {
+    name: string;
+    iso_code: string;
+};
+
+export type Forecast = {
+    shortForecast: string;
+    temperature: number;
+};
+
+export type LocationData = {
+    slug: string,
+    title: string,
+    description: string,
+    city: string | null,
+    latitude: string,
+    longitude: string,
+    stream_type: string,
+    stream_url: string,
+    country: Country;
+    forecast: Forecast;
+};
+
+export type MatchResponse = {
+    success: boolean;
+    message: string;
+    data: LocationData;
+};
+
+export type Preferences = {
+    precipitation: 'none' | 'rain' | 'snow';
+    temperature: 'hot' | 'cold';
+    country: Country;
+};
